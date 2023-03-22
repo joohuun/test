6758a4bf711168132b205ff1ee4dc042a09fb14b
juhoon <k00005781@gmail.com>
2023-03-22 18:28:48 +0900

    test

--- app/test/test_cookie.py
+++ app/test/test_cookie.py
@@ -1,15 +1,16 @@
-import urllib.parse
-import urllib.request
+# import urllib.parse
+# import urllib.request
 
-url = 'http://10.2.1.100:5001/tower/signin'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-values = {'email': 'kimju0612@naver.com',
-          'password': '1234Qwer!!',
-          'language': 'Python' }
-headers = {'User-Agent': user_agent}
+# url = 'http://10.2.1.100:5001/tower/signin'
+# user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+# values = {'email': 'kimju0612@naver.com',
+#           'password': '1234Qwer!!',
+#           'language': 'Python' }
+# headers = {'User-Agent': user_agent}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data, headers)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
+# data = urllib.parse.urlencode(values)
+# data = data.encode('ascii')
+# req = urllib.request.Request(url, data, headers)
+
+# with urllib.request.urlopen(req) as response:
+#     the_page = response.read()
